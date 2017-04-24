cba5ba27a8241db1abe8d735d47a1eab7ab3bfd2
Walpurisnacht <walpurisnacht.klarissa@gmail.c>
2017-04-24 23:09:36 +0700

    Conflict fork pull request

--- src/vn/edu/git/Main.java
+++ src/vn/edu/git/Main.java
@@ -3,18 +3,12 @@ package vn.edu.git;
 import vn.edu.sample.Test;
 
 public class Main {
-    private void function1(int i) {
-        for (i = 0; i < 1000; i++) {
-            System.out.println(i);
-        }
-
-        for (int j = 0; j < 10; j++) {
-            System.out.println(j);
-        }
-    }
-
     private void function2() {
         System.out.print("FUNCTION2");
+    }
+
+    private void function3(String s) {
+        System.out.print(s);
     }
 
     public static void main(String[] args) {
