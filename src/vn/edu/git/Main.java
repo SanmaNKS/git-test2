30f5f2a8cb1ca57e353318920cbc8e983e0e563d
Walpurisnacht <walpurisnacht.klarissa@gmail.c>
2017-04-24 21:56:11 +0700

    Test commit

--- src/vn/edu/git/Main.java
+++ src/vn/edu/git/Main.java
@@ -1,8 +1,11 @@
 package vn.edu.git;
 
+import vn.edu.sample.Test;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.print("Hello World");
+        Test test = new Test();
     }
 }
---
+++ src/vn/edu/sample/Test.java
@@ -0,0 +1,9 @@
+package vn.edu.sample;
+
+/**
+ * Created by walpurisnacht on 24/04/2017.
+ */
+public class Test {
+
+    private int testField;
+}
